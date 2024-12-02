db0c8b827d03fd689dfd809163389079f6e6c6a0
Limit6 <81852696+Limit-sest@users.noreply.github.com>
2024-12-02 19:57:03 +0100

    Color variables

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,7 +4,17 @@ export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
 
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        bgPrimary: "rgb(var(--color-bgPrimary) / <alpha-value>)",
+        bgSecondary: "rgb(var(--color-bgSecondary) / <alpha-value>)",
+        border: "rgb(var(--color-border) / <alpha-value>)",
+        accent: "rgb(var(--color-accent) / <alpha-value>)",
+        accent2: "rgb(var(--color-accent2) / <alpha-value>)",
+        text: "rgb(var(--color-text) / <alpha-value>)",
+        textVibrant: "rgb(var(--color-textVibrant) / <alpha-value>)"
+      }
+    }
   },
 
   plugins: []
