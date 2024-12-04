f7f594a05357ffd63a1c4c83e39664dd1a956d0b
Limit6 <81852696+Limit-sest@users.noreply.github.com>
2024-12-04 20:01:11 +0100

    Finish chip component

--- src/routes/Chip.svelte
+++ src/routes/Chip.svelte
@@ -1,7 +1,7 @@
 <script>
-    let { children } = $props();
+    let { label } = $props();
 </script>
 
-<div class="bg-bgSecondary text-accent2 font-display border-border h-8 sm:h-10 text-sm sm:text-base">
-    {children}
+<div class="bg-bgSecondary text-accent2 font-medium font-display rounded-md border-border px-2 sm:px-3 flex items-center justify-center w-fit h-8 sm:h-10 text-sm sm:text-base">
+    {label}
 </div>
---
+++ src/routes/ChipSmall.svelte
@@ -0,0 +1,7 @@
+<script>
+    let { label } = $props();
+</script>
+
+<div class="bg-bgSecondary text-accent2 font-medium font-display rounded-md border-border px-2 sm:px-3 py-1 sm:py-2 flex items-center justify-center w-fit h-auto text-xs sm:text-sm">
+    {label}
+</div>
