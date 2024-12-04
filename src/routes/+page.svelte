2d5c016442069ff040c7b82da986360d0a51a449
Limit6 <81852696+Limit-sest@users.noreply.github.com>
2024-12-04 21:01:52 +0100

    Add buttons

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,11 @@
 <script>
     import Chip from "./Chip.svelte";
+    import ButtonPrimary from "./ButtonPrimary.svelte";
+    import ButtonSecondary from "./ButtonSecondary.svelte";
 </script>
 
-<Chip>Skill</Chip>
+<div class="p-10">
+    <Chip label="Skill"/>
+    <ButtonPrimary label="Button" href="https://example.com"/>
+    <ButtonSecondary label="Button" href="https://example.com" withIcon="true"/>
+</div>
---
+++ src/routes/ButtonPrimary.svelte
@@ -0,0 +1,9 @@
+<script>
+    let { label, href } = $props();
+</script>
+
+<a href="{href}" class="bg-accent/5 text-accent font-bold font-display rounded-md border-border border px-3 py-2 flex items-center justify-center gap-1 w-fit h-auto sm:h-10 text-sm sm:text-base">
+    {label}
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-arrow-up-right hidden sm:block"><path d="M7 7h10v10"/><path d="M7 17 17 7"/></svg>
+    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.75" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-arrow-up-right block sm:hidden"><path d="M7 7h10v10"/><path d="M7 17 17 7"/></svg>
+</a>
