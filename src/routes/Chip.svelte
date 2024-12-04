79c01ee185e8f11dc200a8f41f7cecb48d2bd03a
Limit6 <81852696+Limit-sest@users.noreply.github.com>
2024-12-04 21:02:15 +0100

    Fix border not visible

--- src/routes/Chip.svelte
+++ src/routes/Chip.svelte
@@ -2,6 +2,6 @@
     let { label } = $props();
 </script>
 
-<div class="bg-bgSecondary text-accent2 font-medium font-display rounded-md border-border px-2 sm:px-3 flex items-center justify-center w-fit h-8 sm:h-10 text-sm sm:text-base">
+<div class="bg-bgSecondary text-accent2 font-medium font-display rounded-md border-border border px-2 sm:px-3 flex items-center justify-center w-fit h-8 sm:h-10 text-sm sm:text-base">
     {label}
 </div>
--- src/routes/ChipSmall.svelte
+++ src/routes/ChipSmall.svelte
@@ -2,6 +2,6 @@
     let { label } = $props();
 </script>
 
-<div class="bg-bgSecondary text-accent2 font-medium font-display rounded-md border-border px-2 sm:px-3 py-1 sm:py-2 flex items-center justify-center w-fit h-auto text-xs sm:text-sm">
+<div class="bg-bgSecondary text-accent2 font-medium font-display rounded-md border-border border px-2 sm:px-3 py-1 sm:py-2 flex items-center justify-center w-fit h-auto text-xs sm:text-sm">
     {label}
 </div>
