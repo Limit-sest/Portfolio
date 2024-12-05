47f65017eb988544df943b85c3b653662be59189
Limit-sest <81852696+Limit-sest@users.noreply.github.com>
2024-12-05 12:26:48 +0100

    Add basic header

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,11 @@
 	import '../app.css';
 	let { children } = $props();
 </script>
+
+<header class="bg-bgSecondary border-border border-t md:border-b  h-12 w-full flex justify-between items-center px-4 font-display font-bold text-sm md:text-base text-text">
+    <p>Antonín Kučera</p>
+</header>
+
 <div class="bg-bgPrimary">
     {@render children()}
 </div>
