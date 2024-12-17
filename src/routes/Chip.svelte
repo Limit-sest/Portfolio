00fc58b3000f97a87da976460d5d3ad8cb8316a9
Limit-sest <81852696+Limit-sest@users.noreply.github.com>
2024-12-17 09:00:23 +0100

    Delete old design components

--- src/routes/Chip.svelte
+++
@@ -1,7 +0,0 @@
-<script>
-    let { label } = $props();
-</script>
-
-<div class="bg-bgSecondary text-accent2 font-medium font-display rounded-md border-border border px-2 md:px-3 flex items-center justify-center w-fit h-8 md:h-10 text-sm md:text-base">
-    {label}
-</div>
