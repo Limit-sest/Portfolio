3623ddb35989e213ceadeafe981924d895cbf3c3
Limit-sest <81852696+Limit-sest@users.noreply.github.com>
2024-12-18 10:05:58 +0100

    Update outline button

--- src/lib/ui/ButtonOutline.svelte
+++ src/lib/ui/ButtonOutline.svelte
@@ -2,6 +2,6 @@
     let { label, href } = $props();
 </script>
 
-<a href="{href}" aria-label="{label}" class="font-display font-medium text-fgDim | flex justify-center items-centerwww h-12 px-3 | rounded-full border border-fgDim">
+<a href="{href}" aria-label="{label}" class="font-display font-medium text-fgDim | flex justify-center items-center h-12 px-3 | rounded-full border border-fgDim">
     {label}
 </a>
