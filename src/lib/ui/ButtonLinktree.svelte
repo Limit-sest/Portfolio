5b4c9f1f708c58d8411ea65a10ce3f28df9b8979
Limit-sest <81852696+Limit-sest@users.noreply.github.com>
2024-12-20 10:34:45 +0100

    Add ButtonLinktree.svelte

---
+++ src/lib/ui/ButtonLinktree.svelte
@@ -0,0 +1,9 @@
+<script>
+    let { label, href, name } = $props();
+</script>
+
+<a href="{href}" aria-label="{name} link button" class="flex justify-start lg:justify-between items-center h-12 px-3 | rounded-full bg-fgDim | transition-colors hover:bg-bg">
+    <div class="">
+
+    </div>
+</a>
