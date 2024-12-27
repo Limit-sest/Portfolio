69f9d436068e978f0ed0838efc05780c303eb2fc
Limit6 <81852696+Limit-sest@users.noreply.github.com>
2024-12-27 10:36:31 +0100

    Add header

--- src/lib/ui/ButtonLinktree.svelte
+++ src/lib/ui/ButtonLinktree.svelte
@@ -1,9 +1,20 @@
 <script>
-    let { label, href, name } = $props();
+    import Github from 'virtual:icons/simple-icons/github'
+    import Discord from 'virtual:icons/simple-icons/discord'
+    import Mail from 'virtual:icons/lucide/mail'
+    let { label, href, username } = $props();
 </script>
 
-<a href="{href}" aria-label="{name} link button" class="flex justify-start lg:justify-between items-center h-12 px-3 | rounded-full bg-fgDim | transition-colors hover:bg-bg">
-    <div class="">
-
+<a href="{href}" aria-label="{label} link button" class="flex justify-start md:justify-between gap-4 items-center h-12 px-6 | rounded-full bg-fgDim | transition-colors hover:bg-fg">
+    <div class="flex items-center gap-4">
+        {#if label === 'Github'}
+            <Github class="w-8 h-auto text-bg/80"/>
+        {:else if label === 'Discord'}
+            <Discord class="w-8 h-auto text-bg/80"/>
+        {:else if label === 'Email'}
+            <Mail class="w-8 h-auto text-bg/80"/>
+        {/if}
+        <p class="uppercase font-display font-medium text-bg/80 hidden md:block">{label}</p>
     </div>
+    <p aria-label="username" class="font-display font-bold text-bg">{username}</p>
 </a>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,23 @@
 <script>
+    import ButtonOutline from "$lib/ui/ButtonOutline.svelte";
+    import ButtonLinktree from "$lib/ui/ButtonLinktree.svelte";
+    import ArrowDown from "virtual:icons/lucide/arrow-down"
 
-import ButtonOutline from "$lib/ui/ButtonOutline.svelte";
 </script>
 
-<div class="p-10">
-    <ButtonOutline label="Go to home" href="/" />
-</div>
+<main class="p-4 flex flex-col gap-4 md:gap-6 max-w-3xl mx-auto">
+    <section class="relative flex flex-col items-center w-full my-32">
+        <p class="font-display text-3xl md:text-4xl text-fgDim mb-2 z-20 mt-4">Hello! I'm</p>
+        <h1 class="font-display font-bold text-4xl md:text-7xl text-fg mb-6 z-20">Antonín Kučera</h1>
+        <p class="font-sans font-semibold text-center text-base md:text-lg text-fgDim z-20 max-w-md mb-4">I’m a designer and programmer that enjoys making creative things. I currently study cybersecurity in Czechia.</p>
+        <div class="absolute top-0 left-0 bg-accent/20 z-10 w-full h-full blur-3xl"></div>
+    </section>
+
+    <ArrowDown class="w-10 h-10 text-fgDim mx-auto" />
+
+    <section>
+        <h2>Things I've built</h2>
+
+    </section>
+
+</main>
