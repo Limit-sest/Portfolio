5be2d65e1a96fc921199a90585eb9d4b18e462ea
Limit6 <81852696+Limit-sest@users.noreply.github.com>
2025-01-01 13:56:36 +0100

    Add welcome text

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,6 +7,8 @@
 </script>
 
 <main class="px-4 pt-20 flex flex-col md:grid md:grid-cols-2 gap-4 md:gap-6 max-w-4xl mx-auto">
+    <h2 class="col-span-2 text-center text-fgDim font-display text-3xl my-8">Welcome to my portfolio!</h2>
+
     <section aria-label="About me" class="w-full relative">
         <PersonalCard></PersonalCard>
     </section>
