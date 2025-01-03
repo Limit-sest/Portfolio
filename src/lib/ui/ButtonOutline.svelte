9a1424e0267c1a7640f4f62a744fa0c1751d9ba5
Limit6 <81852696+Limit-sest@users.noreply.github.com>
2025-01-03 11:40:38 +0100

    Edit hover effect

--- src/lib/ui/ButtonOutline.svelte
+++ src/lib/ui/ButtonOutline.svelte
@@ -2,6 +2,6 @@
     let { label, href} = $props();
 </script>
 
-<a href="{href}" aria-label="{label} link button" class="py-1.5 px-4 | rounded-full | transition-all hover:bg-accent/30 font-display font-medium text-fgDim text-sm border border-fgDim">
+<a href="{href}" aria-label="{label} link button" class="py-1.5 px-4 | rounded-full | transition-all hover:bg-accent/30 font-display font-medium text-fgDim hover:text-fg text-sm border border-fgDim hover:border-fg">
     {label}
 </a>
