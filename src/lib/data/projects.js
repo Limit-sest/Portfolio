d2421f5de1bbc35d65ee9e70ea9b937eb617d116
Limit-sest <81852696+Limit-sest@users.noreply.github.com>
2025-01-07 09:27:59 +0100

    Add new project

--- src/lib/data/projects.js
+++ src/lib/data/projects.js
@@ -6,13 +6,26 @@ export const projects = [
     "year": 2024,
     "quality": 5,
     "description": "A python terminal app for managing personal expenses. It was built using the Textual library.",
-    "personalNote": "This app was built during High Seas event from Hackclub.",
     "tags": [
       "Programming"
     ],
     "buttons": [
       { "label": "Live demo", "href": "https://textual-web.io/limit6/expense_tracker"},
       { "label": "Github", "href": "https://github.com/Limit-sest/Expense_tracker" },
+    ]
+  },
+  {
+    "title": "Bakaláři calendar",
+    "id": "bakalari_calendar",
+    "imgAlt": "A showcase image of the generated calendar events.",
+    "year": 2024,
+    "quality": 6,
+    "description": "Python script that fetches timetable from the Bakaláři API and generates calendar events.",
+    "tags": [
+      "Programming"
+    ],
+    "buttons": [
+      { "label": "Github", "href": "https://github.com/Limit-sest/Bakalari-calendar" },
     ]
   }
 ]
