c7ba0cc2d196c6efe5b0712be4ed1e73ade443e0
Limit-sest <81852696+Limit-sest@users.noreply.github.com>
2025-01-07 09:31:26 +0100

    Make year value string

--- src/lib/data/projects.js
+++ src/lib/data/projects.js
@@ -3,7 +3,7 @@ export const projects = [
     "title": "TUI Expense tracker",
     "id": "tui_expense_tracker",
     "imgAlt": "A screenshot from the TUI app",
-    "year": 2024,
+    "year": "2024",
     "quality": 5,
     "description": "A python terminal app for managing personal expenses. It was built using the Textual library.",
     "tags": [
@@ -18,7 +18,7 @@ export const projects = [
     "title": "Bakaláři calendar",
     "id": "bakalari_calendar",
     "imgAlt": "A showcase image of the generated calendar events.",
-    "year": 2024,
+    "year": "2024",
     "quality": 6,
     "description": "Python script that fetches timetable from the Bakaláři API and generates calendar events.",
     "tags": [
@@ -27,5 +27,6 @@ export const projects = [
     "buttons": [
       { "label": "Github", "href": "https://github.com/Limit-sest/Bakalari-calendar" },
     ]
-  }
+  },
+
 ]
