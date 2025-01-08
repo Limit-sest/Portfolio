e1da01ff93b452848cfdd7060fb932b04ef342b0
Limit-sest <81852696+Limit-sest@users.noreply.github.com>
2025-01-08 14:23:53 +0100

    Start work on footer

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,24 @@
 <script lang="ts">
 	import '../app.css';
 	let { children } = $props();
+
+    const webLinks = [
+        {
+            label: "Source code",
+            href: "https://github.com/Limit-sest/Portfolio"
+        }
+    ]
 </script>
 
 <div class="bg-bg">
     {@render children()}
 </div>
 
+<footer class="bg-bgDim w-full p-4">
+    <div class="p-4 grid grid-cols-2 gap-4 md:gap-6 max-w-4xl mx-auto">
+        <div class="flex flex-col gap-2">
+            <p class="text-sm font-display font-medium">WEBSITE</p>
+
+        </div>
+    </div>
+</footer>
